b882f955c95cff20537b4bc4715b7c0c2c28cd6f
Silas Eduardo <silasn1@live.com>
2025-11-12 22:02:30 -0300

    criacao do projeto inicial

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+app.use(router);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
